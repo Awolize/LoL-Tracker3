8f351e52d4f79a3f5e2bb8ccf820c5abcea1970e
Awolize <alex.store@hotmail.com>
2025-10-29 22:44:26 +0100

    cleanup dockerfile env

--- app/src/components/custom/header.tsx
+++ app/src/components/custom/header.tsx
@@ -13,134 +13,135 @@ import { FullSummonerUpdate } from "./full-summoner-update";
 import { ScaleSlider } from "./scale-slider";
 
 export enum SortOrder2 {
-	Points = 0,
-	AZ = 1,
-	Level = 2,
+  Points = 0,
+  AZ = 1,
+  Level = 2,
 }
 
 export default function Header({
-	champions,
+  champions,
 }: {
-	champions: CompleteChampionInfo[];
+  champions: CompleteChampionInfo[];
 }) {
-	const {
-		showLevels,
-		showMasteryPoints,
-		byRole,
-		filterPoints,
-		sortOrder,
-		showSelectedChampions,
-		selectedChampions,
-		setSortOrder,
-		setFilterPoints,
-		toggleLevels,
-		toggleMasteryPoints,
-		toggleSortedByRole,
-		toggleShowSelectedChampions,
-	} = useOptionsPersistentContext((state) => state);
-	const toggleShowMatchHistory = useMatchHistoryStore(
-		(state) => state.toggleShowMatchHistory,
-	);
+  const {
+    showLevels,
+    showMasteryPoints,
+    byRole,
+    filterPoints,
+    sortOrder,
+    showSelectedChampions,
+    selectedChampions,
+    setSortOrder,
+    setFilterPoints,
+    toggleLevels,
+    toggleMasteryPoints,
+    toggleSortedByRole,
+    toggleShowSelectedChampions,
+  } = useOptionsPersistentContext((state) => state);
+  const toggleShowMatchHistory = useMatchHistoryStore(
+    (state) => state.toggleShowMatchHistory,
+  );
 
-	const user = useUserContext((s) => s.user);
+  const user = useUserContext((s) => s.user);
 
-	const filteredChoices: Choice[] = [
-		{ text: "100", value: 100 },
-		{ text: "500", value: 500 },
-		{ text: "1,000", value: 1000 },
-		{ text: "1,800 (Level 2)", value: 1800 },
-		{ text: "5,000", value: 5000 },
-		{ text: "6000 (Level 3)", value: 6000 },
-		{ text: "10,000", value: 10000 },
-		{ text: "12,600 (Level 4)", value: 12600 },
-		{ text: "21,600 (Level 5)", value: 21600 },
-		{ text: "50,000", value: 50000 },
-		{ text: "100,000", value: 100000 },
-		{
-			text: "All",
-			value: Number.MAX_SAFE_INTEGER,
-		},
-	];
+  const filteredChoices: Choice[] = [
+    { text: "100", value: 100 },
+    { text: "500", value: 500 },
+    { text: "1,000", value: 1000 },
+    { text: "1,800 (Level 2)", value: 1800 },
+    { text: "5,000", value: 5000 },
+    { text: "6000 (Level 3)", value: 6000 },
+    { text: "10,000", value: 10000 },
+    { text: "12,600 (Level 4)", value: 12600 },
+    { text: "21,600 (Level 5)", value: 21600 },
+    { text: "50,000", value: 50000 },
+    { text: "75,600", value: 76500 },
+    { text: "100,000", value: 100000 },
+    {
+      text: "All",
+      value: Number.MAX_SAFE_INTEGER,
+    },
+  ];
 
-	const sortOrderChoices: Choice[] = [
-		{ text: "Points", value: SortOrder2.Points },
-		{ text: "A-Z", value: SortOrder2.AZ },
-		{ text: "Level", value: SortOrder2.Level },
-	];
+  const sortOrderChoices: Choice[] = [
+    { text: "Points", value: SortOrder2.Points },
+    { text: "A-Z", value: SortOrder2.AZ },
+    { text: "Level", value: SortOrder2.Level },
+  ];
 
-	// Exclude hidden champions from counts
-	const visibleChampions = champions.filter(
-		(c) => !selectedChampions.has(c.id),
-	);
+  // Exclude hidden champions from counts
+  const visibleChampions = champions.filter(
+    (c) => !selectedChampions.has(c.id),
+  );
 
-	const filteredCount = visibleChampions.filter((c) =>
-		filteredOut(c, filterPoints),
-	).length;
+  const filteredCount = visibleChampions.filter((c) =>
+    filteredOut(c, filterPoints),
+  ).length;
 
-	return (
-		<div className="flex w-full items-center justify-between px-4 py-2">
-			{/* Left */}
-			<div className="flex flex-1 justify-start">
-				<DifferentHeaderCounter
-					finished={filteredCount}
-					total={visibleChampions.length}
-					version={0}
-				/>
-			</div>
-			<div className="flex flex-row items-center justify-center gap-4 mx-auto">
-				<FullSummonerUpdate user={user} />
-				<div className="h-8 w-px bg-gray-500" />
-				<SwitchWithLabel
-					label={"By role"}
-					checked={byRole}
-					onChange={toggleSortedByRole}
-				/>
-				<Dropdown
-					callback={(choice) => setFilterPoints(choice)}
-					menuLabel="Filter by"
-					// biome-ignore lint/style/noNonNullAssertion: This will always find a match
-					choice={filteredChoices.find((el) => el.value === filterPoints)!}
-					choices={filteredChoices}
-				/>
-				<Dropdown
-					choices={sortOrderChoices}
-					menuLabel="Sort by"
-					// biome-ignore lint/style/noNonNullAssertion: This will always find a match
-					choice={sortOrderChoices.find((el) => el.value === sortOrder)!}
-					callback={(value) => setSortOrder(value)}
-				/>
-				<ToggleEye
-					label="Hide selected champions"
-					checked={!showSelectedChampions}
-					onChange={toggleShowSelectedChampions}
-				/>
-				<div className="h-8 w-[1px] bg-gray-500" />
-				<SwitchWithLabel
-					label={"Mastery Points"}
-					checked={showMasteryPoints}
-					onChange={toggleMasteryPoints}
-				/>
-				<SwitchWithLabel
-					label={"Levels"}
-					checked={showLevels}
-					onChange={toggleLevels}
-				/>
-				<div className="flex flex-col items-center gap-3">
-					<Label>Image size</Label>
-					<ScaleSlider />
-				</div>
-				<div className="h-8 w-[1px] bg-gray-500" />
-				<Button
-					size={"sm"}
-					variant="secondary"
-					className="w-32"
-					onClick={toggleShowMatchHistory}
-				>
-					Match history
-				</Button>
-			</div>
-			{/* Right (optional, can be empty) */}
-			<div className="flex flex-1 justify-end" />
-		</div>
-	);
+  return (
+    <div className="flex w-full items-center justify-between px-4 py-2">
+      {/* Left */}
+      <div className="flex flex-1 justify-start">
+        <DifferentHeaderCounter
+          finished={filteredCount}
+          total={visibleChampions.length}
+          version={0}
+        />
+      </div>
+      <div className="flex flex-row items-center justify-center gap-4 mx-auto">
+        <FullSummonerUpdate user={user} />
+        <div className="h-8 w-px bg-gray-500" />
+        <SwitchWithLabel
+          label={"By role"}
+          checked={byRole}
+          onChange={toggleSortedByRole}
+        />
+        <Dropdown
+          callback={(choice) => setFilterPoints(choice)}
+          menuLabel="Filter by"
+          // biome-ignore lint/style/noNonNullAssertion: This will always find a match
+          choice={filteredChoices.find((el) => el.value === filterPoints)!}
+          choices={filteredChoices}
+        />
+        <Dropdown
+          choices={sortOrderChoices}
+          menuLabel="Sort by"
+          // biome-ignore lint/style/noNonNullAssertion: This will always find a match
+          choice={sortOrderChoices.find((el) => el.value === sortOrder)!}
+          callback={(value) => setSortOrder(value)}
+        />
+        <ToggleEye
+          label="Hide selected champions"
+          checked={!showSelectedChampions}
+          onChange={toggleShowSelectedChampions}
+        />
+        <div className="h-8 w-[1px] bg-gray-500" />
+        <SwitchWithLabel
+          label={"Mastery Points"}
+          checked={showMasteryPoints}
+          onChange={toggleMasteryPoints}
+        />
+        <SwitchWithLabel
+          label={"Levels"}
+          checked={showLevels}
+          onChange={toggleLevels}
+        />
+        <div className="flex flex-col items-center gap-3">
+          <Label>Image size</Label>
+          <ScaleSlider />
+        </div>
+        <div className="h-8 w-[1px] bg-gray-500" />
+        <Button
+          size={"sm"}
+          variant="secondary"
+          className="w-32"
+          onClick={toggleShowMatchHistory}
+        >
+          Match history
+        </Button>
+      </div>
+      {/* Right (optional, can be empty) */}
+      <div className="flex flex-1 justify-end" />
+    </div>
+  );
 }
